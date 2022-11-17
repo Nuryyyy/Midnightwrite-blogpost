86fd51d6f9cfcca8d80b5552e32550a0cca32af0
Nuryyyy <a.nurymar@gmail.com>
2022-11-17 20:55:12 +0800

    persist login

--- Frontend/reactjs/src/App.js
+++ Frontend/reactjs/src/App.js
@@ -18,7 +18,6 @@ function App() {
 
   return (
     <BrowserRouter>
-   
     <Routes>
       <Route path='/' element={<Layout />}>
       <Route path="/" element={<LandingPage />} />
--- Frontend/reactjs/src/hooks/useRefreshToken.js
+++ Frontend/reactjs/src/hooks/useRefreshToken.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import axios from '../api/axios'
 import useAuth from './useAuth'
 
@@ -13,7 +12,7 @@ function useRefreshToken() {
         setAuth(prev => {
             console.log(JSON.stringify(prev))
             console.log(response.data.accessToken)
-            return{...prev, token: response.data.accessToken}
+            return{...prev, accessToken: response.data.accessToken}
         })
         return response.data.accessToken
     }
